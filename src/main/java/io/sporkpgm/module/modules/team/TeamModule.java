56b4ad3fa0ab8234ae7a45e3d4dfd2e4763ca0f0
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2014-04-30 10:48:48 +0100

    Fix small error.

--- src/main/java/io/sporkpgm/module/modules/team/TeamModule.java
+++ src/main/java/io/sporkpgm/module/modules/team/TeamModule.java
@@ -3,7 +3,7 @@ package io.sporkpgm.module.modules.team;
 import io.sporkpgm.module.Module;
 import io.sporkpgm.module.ModuleInfo;
 
-@ModuleInfo(description = "Handles the teams that the players can join")
+@ModuleInfo(description = "Handles the teams that the players can join", builder = TeamBuilder.class)
 public class TeamModule extends Module {
 
 
