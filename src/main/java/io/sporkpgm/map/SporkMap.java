e8e57d6d32a95868a69d0372594314f764301715
ParaPenguin <harry.fox.ic@icloud.com>
2014-04-30 21:51:26 +0100

    TeamCollection

--- src/main/java/io/sporkpgm/map/SporkMap.java
+++ src/main/java/io/sporkpgm/map/SporkMap.java
@@ -1,13 +1,19 @@
 package io.sporkpgm.map;
 
+import io.sporkpgm.module.modules.team.TeamCollection;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class SporkMap {
 
 	private Scoreboard scoreboard;
+	private TeamCollection teams;
 
 	public Scoreboard getScoreboard() {
 		return scoreboard;
+	}
+
+	public TeamCollection getTeams() {
+		return teams;
 	}
 
 }
---
+++ src/main/java/io/sporkpgm/module/modules/team/TeamCollection.java
@@ -0,0 +1,82 @@
+package io.sporkpgm.module.modules.team;
+
+import io.sporkpgm.map.SporkMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamCollection {
+
+	private SporkMap map;
+	private TeamModule observers;
+	private List<TeamModule> teams;
+
+	public TeamCollection(SporkMap map, List<TeamModule> teams) {
+		this.map = map;
+		this.observers = TeamBuilder.observers(map);
+		this.teams = new ArrayList<>();
+		for(TeamModule team : teams) {
+			this.teams.add(team);
+		}
+	}
+
+	public SporkMap getMap() {
+		return map;
+	}
+
+	public TeamModule getObservers() {
+		return observers;
+	}
+
+	public List<TeamModule> getTeams() {
+		return teams;
+	}
+
+	public List<TeamModule> getTeams(String search) {
+		List<TeamModule> test = new ArrayList<>();
+		test.addAll(getTeams());
+		test.add(getObservers());
+
+		List<TeamModule> teams = new ArrayList<>();
+		for(TeamModule team : test) {
+			if(!teams.contains(team)) {
+				String name = team.getName().toLowerCase();
+				String colour = team.getColor().name().replace("_", " ").toLowerCase();
+				if(name.equalsIgnoreCase(search) || colour.equalsIgnoreCase(search)) {
+					teams.add(team);
+				}
+			}
+		}
+
+		if(teams.size() > 0) {
+			return teams;
+		}
+
+		for(TeamModule team : test) {
+			if(!teams.contains(team)) {
+				String name = team.getName().toLowerCase();
+				String colour = team.getColor().name().replace("_", " ").toLowerCase();
+				if(name.startsWith(search.toLowerCase()) || colour.startsWith(search.toLowerCase())) {
+					teams.add(team);
+				}
+			}
+		}
+
+		if(teams.size() > 0) {
+			return teams;
+		}
+
+		for(TeamModule team : test) {
+			if(!teams.contains(team)) {
+				String name = team.getName().toLowerCase();
+				String colour = team.getColor().name().replace("_", " ").toLowerCase();
+				if(name.contains(search.toLowerCase()) || colour.contains(search.toLowerCase())) {
+					teams.add(team);
+				}
+			}
+		}
+
+		return teams;
+	}
+
+}
